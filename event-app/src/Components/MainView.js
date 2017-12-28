1f3c3693e22f3236f61c8cd4c205ae0a0f9506b8
BDesBrisay <bryce@desbrisay.com>
2017-12-28 03:50:28 -0800

    added starter info to other tabs

--- event-app/src/Components/MainView.js
+++ event-app/src/Components/MainView.js
@@ -6,6 +6,9 @@ const MainView = ({ activeTab }) => {
   return (
     <div className='mainView'>
       {activeTab === 'Home' && <EventList />}
+      {activeTab === 'Friends' && <h1>Friends List</h1>}
+      {activeTab === 'Relations' && <h1>Relations List</h1>}
+      {activeTab === 'Profile' && <h1>Profile</h1>}
     </div>
   );
 }
--- event-app/src/Components/TabNav.js
+++ event-app/src/Components/TabNav.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './TabNav.css';
 
 const TabNav = ({ tabs, activeTab, tabHandler }) => {
