d6ec0f03784c3ee4d7c2f39cf9774d68084f0830
BDesBrisay <bryce@desbrisay.com>
2018-01-07 23:45:45 +0800

    faded tags overflow

--- event-app/src/Components/EventItem.js
+++ event-app/src/Components/EventItem.js
@@ -18,9 +18,9 @@ const EventItem = ({ event }) => {
             <p className='bottomLabel'>{event.location}</p>
           </div>
           {('tags' in event && event.tags.length > 0) &&
-            <div className='bottomContain'>
+            <div className='tagsContain'>
               {event.tags.map((tag, index) => (
-                <div key={index} className='tagContain'>{tag}</div>
+                <div key={index} className='tag'>{tag}</div>
               ))}
             </div>
           }
