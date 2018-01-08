f980bd5b14c34a944aefd73a99e9ddf32003c55c
BDesBrisay <bryce@desbrisay.com>
2018-01-08 15:02:05 +0800

    added time ico n to friend item

--- event-app/src/Components/FriendItem.js
+++ event-app/src/Components/FriendItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MdChevronRight } from 'react-icons/lib/md';
+import { MdChevronRight, MdAccessTime } from 'react-icons/lib/md';
 import './FriendItem.css';
 
 const FriendItem = ({ person }) => {
@@ -9,7 +9,11 @@ const FriendItem = ({ person }) => {
         {'image' in person && <img src={person.image} className='friendImage' alt={person.name} />}
         <div className='infoContain'>
           {'name' in person && <h4 className='friendName'>{person.name}</h4>}
-          {'metAt' in person && <p className='friendTime'>Met on {person.metAt}</p>}
+          {'metAt' in person &&
+            <div className='bottomContain'>
+              <h3 className='bottomIcon'><MdAccessTime /></h3>
+              <p className='friendTime'>Met on {person.metAt}</p>
+            </div>}
         </div>
         <MdChevronRight className='friendIcon' />
       </div>
